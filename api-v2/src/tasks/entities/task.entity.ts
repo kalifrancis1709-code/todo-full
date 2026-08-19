6fe95bf2ac5294a90bb924d96b6d27a7fb1c8cd2
Martin <class.brainitek24@gmail.com>
2026-08-19 21:17:58 +0000

    nestjs in progress

--- api-v2/src/tasks/entities/task.entity.ts
+++ api-v2/src/tasks/entities/task.entity.ts
@@ -1,1 +1,10 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({notNull: true})
+    designation: string;
+}
