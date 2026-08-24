a49af2e85fc0e898e8774f220cf631f3c771a547
Martin <class.brainitek24@gmail.com>
2026-08-24 20:55:09 +0000

    nest config data base ok

--- api-v2/src/app.module.ts
+++ api-v2/src/app.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
-import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [TasksModule,ConfigModule.forRoot({isGlobal: true}), TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'admin20',
-    database: 'todo-nest',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  })],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api-v2/src/tasks/entities/task.entity.ts
+++ api-v2/src/tasks/entities/task.entity.ts
@@ -1,10 +1,14 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Task {
-    @PrimaryGeneratedColumn()
+export class Task { 
+    @PrimaryGeneratedColumn() 
     id: number;
 
-    @Column({notNull: true})
-    designation: string;
+    @Column()
+    designation: string; 
+
+    constructor(designation: string) {
+        this.designation = designation;
+    }
 }
