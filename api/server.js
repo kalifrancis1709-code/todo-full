8fe984509122970f7c845077358d6eb7392da38f
Martin <class.brainitek24@gmail.com>
2026-08-31 23:50:30 +0000

    merging main

--- api/server.js
+++ api/server.js
@@ -1,12 +1,6 @@
 import app from "./app.js";
-<<<<<<< HEAD
-
 const PORT = process.env.PORT || 3000;
 
-=======
-const PORT = process.env.PORT || 3000;
-
->>>>>>> main
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
